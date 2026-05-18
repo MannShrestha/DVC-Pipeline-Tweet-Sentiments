8a925debe43b65dd6e066e1f6f3907a5d2f72759
MannShrestha <mannshrestha01@gmail.com>
2026-05-18 19:24:20 +0100

    template created.

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Creating directory 
+mkdir -p src
+mkdir -p research 
+
+# Creating files
+touch src/__init__.py
+touch src/data_ingestion.py 
+touch src/data_preprocessing.py
+touch src/feature_engineering.py
+touch src/model_building.py
+touch src/model_evaluation.py
+touch .env
+touch setup.py
+touch main.py
+touch research/trials.ipynb 
+touch requirements.txt
